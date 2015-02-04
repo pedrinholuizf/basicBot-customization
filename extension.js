bed0637f40fd8904f7cd395e72250723699e006e
pedrinholuizf <pedrinholuizf@gmail.com>
2015-02-03 21:01:43 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,14 +33,15 @@
          */
 
         bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'ip',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat("/me IP do nosso servidor:");
+                    API.sendChat("/me extremesw.g-s.nu");
                 }
             }
         };
